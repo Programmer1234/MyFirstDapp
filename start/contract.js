9d6abc11b4653788de2d50afd4db201a38e86bec
Programmer1234 <shahafgonen73142@gmail.com>
2022-05-24 16:55:14 +0300

    MetaMask "Not Installed" Dapp Flow

--- start/contract.js
+++ start/contract.js
@@ -33,6 +33,32 @@ The `piggybankContract` is compiled from:
 const forwarderOrigin = 'http://localhost:9010'
 
 const initialize = () => {
-  //You will start here 
+  // Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+
+  // Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    // Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //------Inserted Code------\\
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      //When the button is clicked we call this function
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = 'Connect';
+    }
+  };
+  MetaMaskClientCheck();
+  //------/Inserted Code------\\
 }
 window.addEventListener('DOMContentLoaded', initialize)
